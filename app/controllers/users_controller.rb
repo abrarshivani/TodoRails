4e4545b02427b3874c8b35d5843f05192a83be75
Froyo <guest@server.fake>
2014-05-21 21:00:31 +0530

    Added registration mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,9 +46,9 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
+        UserMailer.registration_confirmation(@user).deliver
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
         session[:current_user_id] = @user.id+1
